0cadc5a9566cb4d803105063f3070e9f80338433
pavanpawar <68224008+delete01@users.noreply.github.com>
2020-07-24 21:03:06 +0530

    Day 5 assignment 1

--- Day 5/script3.js
+++ Day 5/script3.js
@@ -1,7 +1,7 @@
 console.log('Fetching API');
 
 // https://jsonplaceholder.typicode.com/todos
-
+//using pormises 
 //Making get request
 
 fetch('https://jsonplaceholder.typicode.com/todos')
@@ -9,7 +9,11 @@ fetch('https://jsonplaceholder.typicode.com/todos')
 .then(data => console.log(data))
 
 
-//using pormises 
+//using async/await
 
-console.log('Promises');
-
+async function feth() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+  
+    console.log('data using async await : ' + data);
+}
